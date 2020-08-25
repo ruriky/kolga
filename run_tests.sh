62bdc7aa61940e21d2e0d01e9fa459b3ff7bf5c0
Rurik Ylä-Onnenvuori <rurik.yla-onnenvuori@anders.com>
2020-08-25 20:51:14 +0300

    Try running docker-compose tests without Kolga container

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -n "$DOCKER_COMPOSE_PROJECT_DIR" ]; then
-  PROJECT_DIR="--project-directory=$DOCKER_COMPOSE_PROJECT_DIR"
-fi
-
 
 docker-compose down -v
-docker-compose $PROJECT_DIR up -V --abort-on-container-exit --exit-code-from client
+docker-compose up -V --abort-on-container-exit --exit-code-from client "$@"
