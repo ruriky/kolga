8c4a04df5ce56e013dc1b5fc5304983982a72d4a
Rurik Ylä-Onnenvuori <rurik.yla-onnenvuori@anders.com>
2020-08-25 18:42:28 +0300

    Use --project-directory with docker-compose

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -n "$DOCKER_COMPOSE_PROJECT_DIR" ]; then
+  PROJECT_DIR="--project-directory=$DOCKER_COMPOSE_PROJECT_DIR"
+fi
+
 
 docker-compose down -v
-docker-compose up -V --abort-on-container-exit --exit-code-from client
+docker-compose $PROJECT_DIR up -V --abort-on-container-exit --exit-code-from client
