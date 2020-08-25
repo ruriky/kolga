cc3a7919caf5b4f1b0283f93d61a3cac6f172d0f
Rurik Ylä-Onnenvuori <rurik.yla-onnenvuori@anders.com>
2020-08-25 16:01:17 +0300

    Add optional workdir for docker-compose

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+if [ -n "$TEST_WORKDIR" ]; then
+  WORKDIR="--workdir=$TEST_WORKDIR"
+fi
+
 docker-compose down -v
-docker-compose up -V --abort-on-container-exit --exit-code-from client "$@"
+docker-compose up -V $WORKDIR --abort-on-container-exit --exit-code-from client
