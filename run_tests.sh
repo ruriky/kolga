7b80b98e074cf3b70c8cd77071738f1b59948a3d
Rurik Ylä-Onnenvuori <rurik.yla-onnenvuori@anders.com>
2020-08-25 16:06:50 +0300

    Ok, no workdir argumenta available

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -n "$TEST_WORKDIR" ]; then
-  WORKDIR="--workdir=$TEST_WORKDIR"
-fi
 
 docker-compose down -v
-docker-compose up -V $WORKDIR --abort-on-container-exit --exit-code-from client
+docker-compose up -V --abort-on-container-exit --exit-code-from client
